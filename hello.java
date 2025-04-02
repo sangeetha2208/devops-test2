9d4151f4a9f5c80cf4f632e93bb3964b24ab84db
smitha <abc@gmail.com>
2025-04-02 20:53:25 +0530

    Added Java file

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+        // Call the method to print the message
+        printMessage();
+    }
+
+    // Method to print a message
+    public static void printMessage() {
+        System.out.println("Hello, World!");
+    }
+}
